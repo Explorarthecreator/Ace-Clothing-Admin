8385092cdbe2a735d6da44cd12527398a91e42e0
Explorarthecreator <fredadejiemma99@gmail.com>
2024-04-22 09:59:13 +0100

    Create a hook to handle the auth status and also to make some routes private

---
+++ src/hooks/useAuthStatus.js
@@ -0,0 +1,61 @@
+
+import {  useMemo, useState} from "react"
+import { useSelector } from "react-redux"
+
+export const useAuthStatus=()=> {
+    const [logged, setLogged] = useState(false)
+    const [checkingStatus, setCheckingStatus] = useState(true)
+
+
+    const {loggedIn} = useSelector((state)=>state.auth)
+
+//     const isMounted = useRef(true)
+
+//     useEffect(()=>{
+
+//         if(isMounted){
+//             const auth = getAuth()
+//             onAuthStateChanged(auth, (user)=>{
+//                 if(user){
+//                     setLogged(true)
+
+//                 }
+// // console.log('ss');
+//                 setCheckingStatus(false)
+//             })
+//         }
+
+//         return ()=>{
+//             isMounted.current = false
+//         }
+        
+//     },[isMounted])
+
+    // useEffect(()=>{
+    //     const auth= getAuth()
+    //     onAuthStateChanged(auth,(user)=>{
+    //         if(user){
+    //             setLogged(true)
+    //         }
+    //         else{
+    //             setLogged(false)
+    //         }
+    //     })
+        
+
+    //     setCheckingStatus(false)
+    // },[loggedIn])
+
+    useMemo(()=>{
+        console.log(loggedIn);
+        if(loggedIn){
+            setLogged(true)
+        }else{
+            setLogged(false)
+        }
+        
+
+        setCheckingStatus(false)
+    },[loggedIn])
+  return {logged, checkingStatus}
+}
