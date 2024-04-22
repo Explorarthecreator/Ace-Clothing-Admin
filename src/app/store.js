4b48a76b2a185e3468ed43a8217e9e961f088b98
Explorarthecreator <fredadejiemma99@gmail.com>
2024-04-22 10:04:39 +0100

    Registered the auth and order reducer

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 // import counterReducer from '../features/counter/counterSlice';
 import navReducer from '../features/navigation/navSlice'
+import authReducer from '../features/auth/authSlice'
+import orderReducer from '../features/order/orderSlice'
 
 export const store = configureStore({
   reducer: {
     // counter: counterReducer,
-    nav: navReducer
+    nav: navReducer,
+    auth: authReducer,
+    order: orderReducer
   },
 });
