e298c419e5b293a9a091e6bf37c9b8b1bec331c0
Explorarthecreator <fredadejiemma99@gmail.com>
2024-04-30 11:09:56 +0100

    Created the products page

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,8 +1,19 @@
+import { FaPlus } from "react-icons/fa6"
+import { Link } from "react-router-dom"
 
 
 function Products() {
   return (
-    <div>Products</div>
+    <div className="p-5 lg:p-10 text-black">
+      <div className="flex justify-between items-center">
+        <h1 className="text-3xl font-medium">
+          Products
+        </h1>
+        <Link className="btn btn-md bg-black font-bold text-white hover:border-2 hover:border-black hover:bg-transparent hover:shadow-lg" to={'/products/add-product'}>
+          <FaPlus/> Add Product
+        </Link>
+      </div>
+    </div>
   )
 }
 
